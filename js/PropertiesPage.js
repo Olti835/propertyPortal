0df4744d4fa27f6fdbac212bf197b7e1e8dfbd0a
Joni <jonzylfiu5@gmail.com>
2025-12-31 19:01:06 +0100

    Created Properties page, PropertyCard and PropertyModal to add properties.

--- js/PropertiesPage.js
+++ js/PropertiesPage.js
@@ -55,9 +55,17 @@ function generatePropertyCard(id, title, image, location, price, specifics, date
                             <span><b>${price}</b></span>
                         </div>
                         <div class="card-specs">
-                            <span>Icon - ${specifics.bedrooms + specifics.bedrooms} Rooms</span>
-                            <span>Icon - ${specifics.parking} Parking</span>
-                            <span>Icon - ${specifics.area} sqft</span>
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 640 512"><path fill="currentColor" d="M32 32c17.7 0 32 14.3 32 32v256h224V160c0-17.7 14.3-32 32-32h224c53 0 96 43 96 96v224c0 17.7-14.3 32-32 32s-32-14.3-32-32v-32H64v32c0 17.7-14.3 32-32 32S0 465.7 0 448V64c0-17.7 14.3-32 32-32m144 96a80 80 0 1 1 0 160a80 80 0 1 1 0-160"/></svg>
+                                ${specifics.bedrooms + specifics.bedrooms} Rooms</span>
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M22 21V9.61c0-1.322 0-1.984-.344-2.496s-.953-.758-2.17-1.25l-6-2.42C12.752 3.148 12.386 3 12 3s-.752.148-1.486.444l-6 2.42c-1.217.492-1.826.738-2.17 1.25S2 8.288 2 9.61V21m14-2v2m-8-2v2"/><path d="m7.5 14l.243-.97c.363-1.455.545-2.183 1.088-2.606C9.373 10 10.123 10 11.623 10h.754c1.5 0 2.25 0 2.792.424c.543.423.725 1.15 1.088 2.606l.243.97m.5 0H7a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1m-8.5 2.49v.01m7-.01v.01"/></g></svg>
+                                ${specifics.parking} Parking
+                            </span>
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm0-2h14V5H5zm0 0V5z"/></svg>
+                                ${specifics.area} sqft
+                            </span>
                         </div>
                         <p id="date">Posted: <b>${date}</b></p>
                         <div class="view-btn">
